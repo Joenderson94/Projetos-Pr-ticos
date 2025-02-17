c0a5e523509460c6a6cb65c34b2b0f51f4f15c6c
Joenderson Henrique <joendersonchaves1@gmail.com>
2025-02-17 09:16:11 -0300

    teste

--- Automação Excel+Email Py/codigo.py
+++ Automação Excel+Email Py/codigo.py
@@ -8,4 +8,5 @@ tabela_consolidada = pd.DataFrame()
 
 for nome_arquivo in arquivos:
     tabela_vendas = pd.read_csv(os.path.join(caminho, nome_arquivo))
+     #pega coluna da data que está em dias e converte para um formato usual com base na data ref
     print(tabela_vendas)
