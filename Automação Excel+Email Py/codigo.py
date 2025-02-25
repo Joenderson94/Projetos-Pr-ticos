6f45b8efe1712b3b387edc3901bb6de12ffde7e4
Joenderson Henrique <joendersonchaves1@gmail.com>
2025-02-25 00:06:42 -0300

    estudo exel

--- Automação Excel+Email Py/codigo.py
+++ Automação Excel+Email Py/codigo.py
@@ -10,7 +10,7 @@ for nome_arquivo in arquivos:
     tabela_vendas['Data de venda']= pd.to_datetime('01/01/1900') + pd.to_timedelta(tabela_vendas['Data de Venda'], unit='d')
      #pega coluna da data que está em dias e converte para um formato usual com base na data ref
 
-    tabela_consolidada = pd.concat([tabela_consolidada, tabela_vendas])
+    tabela_consolidada = pd.concat(tabela_consolidada, tabela_vendas)
 
 tabela_consolidada= tabela_consolidada.sort_values(by='Data de Venda')
 tabela_consolidada = tabela_consolidada.reset_index(drop=True)
